f5dd3fc9ac55c3a5afcf5a868f60f02797dc9ed5
SadipBhattarai <sadipbhattarai3@gmail.com>
2025-02-07 18:06:52 +0545

    Javascript intro

---
+++ js/index.js
@@ -0,0 +1,10 @@
+console.log("Hello Sadip!");
+
+const person = "Sadip Bhattarai";
+console.log(person);
+
+let person1 = "Sadip";
+console.log(person1);
+
+var person2 = "Susan";
+console.log(person2);
