174b57a32ea0a5ea1e3eb296860a15eeb77e9efb
SadipBhattarai <sadipbhattarai3@gmail.com>
2025-02-09 18:01:51 +0545

    Variables

--- js/index.js
+++ js/index.js
@@ -1,10 +1,38 @@
-console.log("Hello Sadip!");
-
 const person = "Sadip Bhattarai";
 console.log(person);
 
-let person1 = "Sadip";
+//ES5
+var person1 = "Susan Kandel";
+person1 = "Sangit Bhattarai"; // {Override}
 console.log(person1);
 
-var person2 = "Susan";
+//ES6
+const person2 = "Alex";
+//person2 = "John"; //{No Override Allowed}
 console.log(person2);
+
+let person3 = "Mike";
+person3 = "Nathan"; //{Override Allowed}
+console.log(person3);
+
+
+// alert("Hello User");
+
+// const user = prompt("What is your name?");
+// console.log(user);
+
+// const student = confirm("Are you above 18?");
+// console.log(student);
+
+//Write a program that asks user for their name and alerts their name with greeting
+
+
+//1. 
+const user = prompt("What is your name?");
+const greeting = "Welcome to the website, " + user;
+alert(greeting);
+
+
+//2. String Literal
+const visitor = prompt("What is your name?");
+alert(`Welcome, ${visitor}`);
